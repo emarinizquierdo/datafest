9a6c75cd38d51e728cc241c2a8ed94782a9cb308
Edu <e.marin.izquierdo@gmail.com>
2015-03-12 18:25:49 +0100

    mongo updated

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -2,6 +2,9 @@
 
 // Production specific configuration
 // =================================
+
+var env = JSON.parse(process.env.VCAP_SERVICES);
+
 module.exports = {
   // Server IP
   ip:       process.env.VCAP_APP_HOST ||
@@ -18,8 +21,7 @@ module.exports = {
   mongo: {
     uri:    process.env.MONGOLAB_URI || 
             process.env.MONGOHQ_URL ||
-            process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
-            "mongodb://3f978017-fe5d-4d95-8682-6fd61a3e433f:f401d412-6df8-4441-a408-0d1044a16d8e@50.23.230.141:10131/db" ||
+            env['mongodb-2.4'][0].credentials.url ||
             'mongodb://localhost/datafest'
   }
 };
