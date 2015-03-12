0c606cdd067f4d0d59aa78d5e03376afb3ebc7ac
Eduardo MARIN IZQUIERDO <eduardo.marin.contractor@dev.bbva.com>
2015-03-12 16:38:59 +0100

    cron working fine

--- server/api/aire/aire.model.js
+++ server/api/aire/aire.model.js
@@ -4,17 +4,15 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var AireSchema = new Schema({
-  timestamp: Number,
-  ce01: Number,
-  ce02: Number,
-  ce03: Number,
-  parameter: Number,
-  tecnic: Number,
-  period: Number,
-  year: Number,
-  day: Number,
-  hour: Number,
-  value: Number
+    id: String,
+    timestamp: Number,
+    parameter: Number,
+    tecnic: Number,
+    period: Number,
+    value: Number,
+    ce01: Number,
+    ce02: Number,
+    ce03: Number
 });
 
 module.exports = mongoose.model('Aire', AireSchema);
--- server/cron/index.js
+++ server/cron/index.js
@@ -10,12 +10,13 @@ var CSVConverter = require("csvtojson").core.Converter;
 var _cron = {};
 
 _cron.start = function() {
-    var cronJob = cron.job("0 */1 * * * *", consumer, null, true);
+    var cronJob = cron.job("0 0 * * * *", consumer, null, true);
     cronJob.start();
 }
 
 var consumer = function() {
-	console.log('consuming...');
+
+    console.log('consuming...');
 
     var options = {
         host: 'www.mambiente.munimadrid.es',
@@ -23,9 +24,11 @@ var consumer = function() {
     }
 
     var request = http.request(options, function(res) {
-        var data = 'CE01,CE02,CE03,PARAMETER,tecnic,period,year,month,day,hour,v,hour1,v,hour2,v,hour3,v,hour4,v,' +
+
+        var data = 'ce01,ce02,ce03,parameter,tecnic,period,year,month,day,hour0,v,hour1,v,hour2,v,hour3,v,hour4,v,' +
             'hour5,v,hour6,v,hour7,v,hour8,v,hour9,v,hour10,v,hour11,v,hour12,v,hour13,v,hour14,v,hour15,v,hour16,v,' +
-            'hour17,v,hour18,v,hour19,v,hour20,v,hour21,v,hour22,v,hour23,v,hour24,v';
+            'hour17,v,hour18,v,hour19,v,hour20,v,hour21,v,hour22,v,hour23,v';
+
         res.on('data', function(chunk) {
             data += chunk;
         });
@@ -41,10 +44,11 @@ var consumer = function() {
                 if (err) {
                     //err handle
                 }
-                console.log(jsonObj);
+
+                console.log('toparse');
+                sendToDB(parseResponse(jsonObj));
+
             });
-
-
 
         });
     });
@@ -53,5 +57,64 @@ var consumer = function() {
     });
     request.end();
 };
+
+var parseResponse = function(p_input) {
+
+    var _response = [];
+    var _pollutionObject = {};
+
+    for (var i = 0; i < p_input.length; i++) {
+
+        _pollutionObject = {};
+
+        for (var j = 0; j < 24; j++) {
+            _pollutionObject = {
+                parameter: p_input[i].parameter,
+                tecnic: p_input[i].tecnic,
+                period: p_input[i].period,
+                ce01: p_input[i].ce01,
+                ce02: p_input[i].ce02,
+                ce03: p_input[i].ce03,
+                id: '' + p_input[i].ce01 + p_input[i].ce02 + p_input[i].ce03 + p_input[i].parameter + p_input[i].year + p_input[i].month + p_input[i].day + j,
+                timestamp: new Date(p_input[i].year, p_input[i].month - 1, p_input[i].day - 1, j, 0, 0).getTime(),
+                value: p_input[i]['hour' + j]
+            };
+            _response.push(_pollutionObject);
+        }
+
+    }
+
+    return _response;
+};
+
+var sendToDB = function(p_array) {
+
+    console.log('to save');
+
+    var i = 0,
+        length = p_array.length;
+
+    _upsert();
+
+    function _upsert() {
+        Aire.update({
+            id: p_array[i].id
+        }, p_array[i], {
+            upsert: true
+        }, function(err, jellybean, snickers) {
+            if (err) {
+                console.log(err);
+            } else {
+                i++;
+                if (i < length) {
+                    _upsert(p_array[i]);
+                } else {
+                    console.log('dump finished');
+                }
+            }
+        });
+    }
+
+}
 
 module.exports = _cron;
