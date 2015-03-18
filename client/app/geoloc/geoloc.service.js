81c79091a318d2e9bed7581dbbcb758986940c80
Edu <e.marin.izquierdo@gmail.com>
2015-03-18 22:23:39 +0100

    circular route ok

--- client/app/geoloc/geoloc.service.js
+++ client/app/geoloc/geoloc.service.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('datafestApp')
-    .factory('geoloc', function($rootScope, MainMap) {
+    .factory('geoloc', function($rootScope, MainMap, $mdToast) {
 
         var _geoloc = {};
 
@@ -23,33 +23,45 @@ angular.module('datafestApp')
 
             geoloc(function(p_data) {
 
-                var geocoder = new google.maps.Geocoder();
-                var latlng = new google.maps.LatLng(p_data.k, p_data.D);
-                geocoder.geocode({
-                    'latLng': latlng
-                }, function(results, status) {
-                    if (status == google.maps.GeocoderStatus.OK) {
-                        if (results[1]) {
-                            $rootScope.directions.origin = results[1].formatted_address;
-                            $rootScope.directions.destination = $rootScope.directions.origin;
-                            MainMap.calcRoute($rootScope.directions.origin, $rootScope.directions.destination);
-                        }
-                    } else {
-
-                    }
-                });
+                _geoloc.showPoints(p_data.k, p_data.D);
 
             });
 
         };
+
+        _geoloc.showPoints = function(lat, long) {
+
+            var geocoder = new google.maps.Geocoder();
+            var latlng = new google.maps.LatLng(lat, long);
+            geocoder.geocode({
+                'latLng': latlng
+            }, function(results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    if (results[1]) {
+                        $rootScope.directions.origin = results[1].formatted_address;
+                        $rootScope.directions.destination = $rootScope.directions.origin;
+                        MainMap.calcRoute($rootScope.directions.origin, $rootScope.directions.destination);
+                    }
+                } else {
+
+                }
+            });
+        };
+
+        _geoloc.showPoints(40.4378271, -3.6795366);
+
+        $mdToast.show(
+            $mdToast.simple()
+            .content('I show you info from Madrid, but you can touch geoloc button to find you')
+            .position('bottom left')
+            .hideDelay(10000)
+        );
 
         var secureApply = function() {
             if (!$rootScope.$$phase) {
                 $rootScope.$apply();
             }
         }
-
-        _geoloc.GoToRealPos();
 
         return _geoloc;
 
