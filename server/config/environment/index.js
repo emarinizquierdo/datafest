44ad214755ea698ff4d4d13307c86ade1fe631a6
Edu <e.marin.izquierdo@gmail.com>
2015-09-04 18:13:58 +0200

    express updated

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -43,7 +43,6 @@ var all = {
 
 };
 
-
 process.env.NODE_ENV = ( process.env.NODE_ENV && (process.env.NODE_ENV == 'production' || process.env.NODE_ENV == 'development')) ? process.env.NODE_ENV : 'development';
 
 // Export the config object based on the NODE_ENV
